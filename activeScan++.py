a6a7b463c69775be17e7f556f37532de462f81b7
Paul Johnston <paul.johnston@portswigger.net>
2019-03-25 13:57:07 +0000

    Bump version

--- activeScan++.py
+++ activeScan++.py
@@ -31,7 +31,7 @@ try:
 except ImportError:
     print "Failed to load dependencies. This issue may be caused by using the unstable Jython 2.7 beta."
 
-VERSION = "1.0.21"
+VERSION = "1.0.21b"
 FAST_MODE = False
 DEBUG = False
 callbacks = None
