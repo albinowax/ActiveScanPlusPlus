442efe7e8de098c1e957f7a5f94fb5195b825711
James Kettle <james.kettle@portswigger.net>
2024-12-12 15:45:03 +0000

    Enable bulkScan-style invocation of individual checks

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -18,7 +18,7 @@ public class BurpExtender implements IBurpExtender, IExtensionStateListener, Bur
     @Override
     public void initialize(MontoyaApi api) {
         Utilities.montoyaApi = api;
-        //BulkUtilities.registerContextMenu(); // don't offer the bulkScan
+        BulkUtilities.registerContextMenu(); // don't offer the bulkScan
         // api.http().registerHttpHandler(new Tester());
         // api.userInterface().registerContextMenuItemsProvider(new OfferHostnameOverride());
     }
--- src/burp/SuspectTransform.java
+++ src/burp/SuspectTransform.java
@@ -21,14 +21,13 @@ public class SuspectTransform extends ParamScan {
         this.checks.put("template evaluation", this::detectRazorExpression);
         this.checks.put("EL evaluation", this::detectAltExpression);
         this.checks.put("unicode normalisation", this::detectUnicodeNormalisation);
-
         this.confirmCount = 2;
     }
-
+    
     private Pair<String, List<String>> detectUnicodeNormalisation(String base) {
         String leftAnchor = Utilities.randomString(6);
         String rightAnchor = Utilities.randomString(6);
-        return new ImmutablePair<>(leftAnchor+"à"+rightAnchor, Collections.singletonList(leftAnchor+"a"+rightAnchor));
+        return new ImmutablePair<>(leftAnchor+"\u212a"+rightAnchor, Collections.singletonList(leftAnchor+"K"+rightAnchor));
     }
 
     private Pair<String, List<String>> detectQuoteConsumption(String base) {
